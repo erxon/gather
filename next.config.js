3d7c2a69e28fc9de65dd1adade356ac9561f4bca
Ericson Castasus <castasuse@gmail.com>
2023-03-31 15:42:17 +0800

    connect to mongodb

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: "mongodb+srv://e-castasus:Eton110300@cluster0.wbcoa.mongodb.net/?retryWrites=true&w=majority"
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
