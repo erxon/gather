183191e514cd7beeefbeb5a8a39354ca91b4341d
Ericson Castasus <castasuse@gmail.com>
2023-04-14 16:51:31 +0800

    cleaning

--- pages/reportSubmit.js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function ReportSubmit(){
-    const router = useRouter();
-    const report = router.query
-    console.log(report.reporter)
-}
