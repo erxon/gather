005b80abfde26c946368da98432128f06ab33dfa
Ericson Castasus <castasuse@gmail.com>
2023-06-06 15:45:44 +0800

    fix matches.js

--- pages/authority/matches.js
+++ pages/authority/matches.js
@@ -1,23 +1,29 @@
 import { useUser } from "@/lib/hooks";
-import { CircularProgress, Box } from "@mui/material";
+import { CircularProgress, Box, Typography } from "@mui/material";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+function RenderMatches() {
+  return (
+    <Box>
+      <Typography>Matches will be displayed here</Typography>
+    </Box>
+  );
+}
 
 export default function Matches() {
   const [user, { loading }] = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if(!loading && !user){
-        router.push("/login")
+    if (!loading && !user) {
+      router.push("/login");
     }
-
-  }, [user, loading])
+  }, [user, loading]);
 
   if (loading) return <CircularProgress />;
-  
-  return (
-    <>
-      <Box>Display possible matches here</Box>
-    </>
-  );
+  if (user && user.type !== "authority") {
+    return <Typography>Forbidden</Typography>;
+  }
+  if (user) return <RenderMatches />
 }
