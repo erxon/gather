772a1dfa3eef49ac1af805ce1dcf62a6d26766bb
Ericson Castasus <castasuse@gmail.com>
2023-07-03 16:15:48 +0800

    login form validation

--- pages/login.js
+++ pages/login.js
@@ -2,30 +2,45 @@ import React, { useState, useEffect } from "react";
 import Router from "next/router";
 import { useUser } from "@/lib/hooks";
 import TextField from "@mui/material/TextField";
-import {Stack, Typography, Paper } from "@mui/material";
+import {
+  Stack,
+  Typography,
+  Paper,
+  Alert,
+  Collapse,
+  IconButton,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import CloseIcon from "@mui/icons-material/Close";
+import TextFieldWithValidation from "@/components/forms/TextFieldWithValidation";
+import PasswordField from "@/components/forms/PasswordField";
 
 export default function Login() {
   const [user, { mutate }] = useUser();
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
   const [error, setError] = useState({
+    title: "",
     show: false,
     message: "",
   });
+  const [isSubmitted, setFormSubmissionState] = useState(false);
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setValues({ ...values, [name]: value });
+  };
 
-    const body = {
-      username: e.currentTarget.username.value,
-      password: e.currentTarget.password.value,
-    };
+  const handleSubmit = async () => {
+    setFormSubmissionState(true);
 
     const res = await fetch("/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
+      body: JSON.stringify(values),
     });
 
     if (res.status === 401) {
@@ -66,31 +81,57 @@ export default function Login() {
           <AccountCircleIcon />
           <Typography variant="h5">Login</Typography>
         </Stack>
-        <form onSubmit={onSubmit}>
-          <Stack justifyContent="center" spacing={3} useFlexGap sx={{ mb: 3 }}>
-            <TextField
-              variant="outlined"
-              name="username"
-              id="username"
-              type="text"
-              label="username"
-              error={error.show}
-            />
-            <TextField
-              variant="outlined"
-              id="password"
-              name="password"
-              type="password"
-              label="password"
-              error={error.show}
-            />
-            {error.show && <Typography variant="subtitle1" color="red">{error.message}</Typography>}
-          </Stack>
+        <Stack justifyContent="center" spacing={3} useFlexGap sx={{ mb: 3 }}>
+          <TextFieldWithValidation
+            variant="outlined"
+            name="username"
+            id="username"
+            type="text"
+            label="username"
+            changeHandler={handleChange}
+            value={values.username}
+            isSubmitted={isSubmitted}
+          />
+          <PasswordField
+            variant="outlined"
+            id="password"
+            name="password"
+            type="password"
+            label="password"
+            handleChange={handleChange}
+            value={values.password}
+            isSubmitted={isSubmitted}
+          />
+          {
+            <Collapse in={error.show}>
+              <Alert
+                action={
+                  <IconButton
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                      setError({ show: false });
+                    }}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+                severity="error"
+              >
+                {error.message}
+              </Alert>
+            </Collapse>
+          }
+        </Stack>
 
-          <Button fullWidth variant="contained" type="submit">
-            Login
-          </Button>
-        </form>
+        <Button
+          onClick={handleSubmit}
+          fullWidth
+          variant="contained"
+          type="submit"
+        >
+          Login
+        </Button>
       </Paper>
     </>
   );
