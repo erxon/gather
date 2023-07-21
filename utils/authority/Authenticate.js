f0a1984a0913acb4be0f4ecbd228fe5a583082fd
Ericson Castasus <castasuse@gmail.com>
2023-07-21 15:03:46 +0800

    fix

--- utils/authority/Authenticate.js
+++ utils/authority/Authenticate.js
@@ -14,7 +14,7 @@ export default function Authenticate({ children }) {
   }, [user, loading]);
 
   if (loading) return <CircularProgress />;
-  if (user.type !== "authority" || user.status === "unverified")
+  if (user && (user.type !== "authority" || user.status === "unverified"))
     return <Typography>Forbidden</Typography>;
   if (user) return children;
 }
